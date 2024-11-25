ef3f7c508fea0e06666e065ff9cc2c69b7e81810
Sohana-Tisha <22101002@uap-bd.edu>
2024-11-25 14:10:01 +0600

    add urls.py products app

--- fuaari/products/urls.py
+++ fuaari/products/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('create_product/<int:seller_id>/', views.create_product, name='create_product'),
     path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
-    path('product_list/',views.product_list,name='product_list'),
+    path('product_list/',views.productlist,name='product_list'),
 ]
 
