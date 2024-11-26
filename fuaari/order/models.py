db7d32dfb9458b2ba9b23079b01237b3ab494fe0
Niloy Ranjan Nath <niloynath003@gmail.com>
2024-11-26 21:12:01 +0600

    Update models.py order app(creating class)

--- fuaari/order/models.py
+++ fuaari/order/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    quantity=models.IntegerField()
+    order_date=models.DateTimeField(auto_now_add=True)
+    buyer_id=models.ForeignKey(User,on_delete=models.CASCADE, related_name='orders')
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE, related_name='order')
+    address=models.TextField()
+    price=models.ForeignKey(Products,on_delete=models.CASCADE,related_name='orders')
