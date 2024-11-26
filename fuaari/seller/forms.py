7e1d81993313ddb6f9718cb68ce2e49b5315c794
abdullahalmusabbir <108293569+abdullahalmusabbir@users.noreply.github.com>
2024-11-26 20:27:57 +0600

    Update seller app forms.py

--- fuaari/seller/forms.py
+++ fuaari/seller/forms.py
@@ -1,1 +1,9 @@
+from django import forms 
+from .models import *
+
+
+class SellerForm(forms.ModelForm):
+    class Meta:
+        model = Seller
+        fields = ['business_name', 'business_location', 'NID']  
 
