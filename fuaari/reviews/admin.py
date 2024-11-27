9aabfb12033b336e6a464489a8ee67ff248ddd16
Tasnia_Kabir <21201137@uap-bd.edu>
2024-11-27 22:02:42 +0600

    register class admin.py reviews app

--- fuaari/reviews/admin.py
+++ fuaari/reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display=('buyer_id','product_id','rating','comment')
+    search_fields=('buyer_id','product_id')
