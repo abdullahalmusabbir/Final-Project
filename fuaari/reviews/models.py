578a7383f26719c30e905320b30ff504e32e9051
Tasnia_Kabir <21201137@uap-bd.edu>
2024-11-27 22:00:58 +0600

    Update models.py reviews app

--- fuaari/reviews/models.py
+++ fuaari/reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import *
+from products.models import *
 
 # Create your models here.
+class Review(models.Model):
+    buyer_id=models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    product_id=models.ForeignKey(Products, on_delete=models.CASCADE, related_name='reviews')
+    rating=models.IntegerField()
+    comment=models.CharField(max_length=225)
+    
+    
