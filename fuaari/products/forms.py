f31dc17991e43ea2691647cbbbbc4b3c4faa22d3
Sohana-Tisha <22101002@uap-bd.edu>
2024-11-28 21:19:21 +0600

    Update forms.py products app

--- fuaari/products/forms.py
+++ fuaari/products/forms.py
@@ -1,1 +1,7 @@
+from django import forms 
+from .models import *
 
+class ProductsForms(forms.ModelForm):
+    class Meta:
+        model=Products
+        fields=['name','description','avaibility','price','images']
