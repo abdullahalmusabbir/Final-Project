c0ca173a3f92e30e3ba10e279095c80fbc1cf473
Niloy Ranjan Nath <niloynath003@gmail.com>
2024-11-28 21:09:06 +0600

    Update urls.py orders app

--- fuaari/order/urls.py
+++ fuaari/order/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/<int:product_id>/', views.create_order, name='create_order'),
+    path('create/<int:product_id>/<int:buyer_id>/', views.create_order, name='create_order'),
 ]
