3fdc27a1c75c8f61a518cd60da1e9cbd5ba296c1
abdullahalmusabbir <108293569+abdullahalmusabbir@users.noreply.github.com>
2024-11-29 23:36:52 +0600

    Update urls.py

--- fuaari/seller/urls.py
+++ fuaari/seller/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('signup/',views.signup_view,name='signup'),
+    path('signup/',views.sign_up,name='signup'),
+    path('login/',views.log_in,name='login'),
+    path('signout/',views.sign_out,name='signout'),
+    path('deshboard/',views.deshboard,name='deshboard'),
 ]
