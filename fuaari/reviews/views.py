5c36d67771347613177567196113c2b0a0f6173a
Tasnia_Kabir <21201137@uap-bd.edu>
2024-11-29 23:51:39 +0600

    Update views.py reviews app

--- fuaari/reviews/views.py
+++ fuaari/reviews/views.py
@@ -1,4 +1,32 @@
-from .models import Review, Products
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ReviewsForm
+from products.models import Products
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from orders.models import Order
 
-def create_reviews(request, product_id):
-  pass
+@login_required
+def create_reviews(request, product_id,buyer_id):
+    product = get_object_or_404(Products, id=product_id)
+    buyer=get_object_or_404(User, id=buyer_id)
+    # buyer = request.user  # Assuming the buyer is the logged-in user
+    
+    # has_purchased = Order.objects.filter(user=request.user, product_id=product.id).exists()
+
+    # if not has_purchased:
+    #     messages.error(request, "You cannot review a product you haven't purchased.")
+    #     return redirect('product_list')  # Redirect to product list or another appropriate page
+
+    if request.method == 'POST':
+        form = ReviewsForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.buyer_id = buyer  
+            review.product_id = product 
+            review.save()
+            return redirect('product_list')  
+    else:
+        form = ReviewsForm()
+
+    return render(request, 'reviews/create_reviews.html', {'form': form, 'product': product})
