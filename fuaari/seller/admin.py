725688b508bf74df310c5fbf20586fde05484d18
abdullahalmusabbir <108293569+abdullahalmusabbir@users.noreply.github.com>
2024-11-29 23:38:25 +0600

    Update admin.py

--- fuaari/seller/admin.py
+++ fuaari/seller/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'business_name', 'business_location', 'NID', 'get_phone_number')
+    search_fields = ('user__username', 'business_name', 'NID')
+    list_filter = ('business_location',)
+
+    def get_phone_number(self, obj):
+        return obj.user.profile.phone_number if hasattr(obj.user, 'profile') else "N/A"
+    get_phone_number.short_description = 'Phone Number'
+
+admin.site.register(Seller, SellerAdmin)
+admin.site.register(Profile)  
