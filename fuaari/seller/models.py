831a681229f5a9df42291f36803a4b93156a9b01
abdullahalmusabbir <108293569+abdullahalmusabbir@users.noreply.github.com>
2024-11-29 23:37:20 +0600

    Update models.py

--- fuaari/seller/models.py
+++ fuaari/seller/models.py
@@ -13,6 +13,7 @@ class Seller(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    purchased_products = models.JSONField(default=list)
 
     def __str__(self):
         return self.user.username    
