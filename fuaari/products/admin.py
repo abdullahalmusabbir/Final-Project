6e81dd3446fc58a8f0137c24cdac8b5dc4ee5af8
Sohana-Tisha <22101002@uap-bd.edu>
2024-11-29 23:42:30 +0600

    Update admin.py products app

--- fuaari/products/admin.py
+++ fuaari/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Products)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description','avaibility','category','price','images')
+    search_fields = ('name','price')
