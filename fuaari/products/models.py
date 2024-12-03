12634003e712a93b933a886a2ebffa70fe1e8a4f
Sohana-Tisha <22101002@uap-bd.edu>
2024-12-03 10:14:49 +0600

    Update models.py

--- fuaari/products/models.py
+++ fuaari/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from seller.models import Seller
 
 # Create your models here.
+class Products(models.Model):
+    name=models.CharField(max_length=225)
+    description=models.CharField(max_length=1024)
+    avaibility=models.IntegerField()
+    price=models.FloatField()
+    images=models.ImageField(upload_to='photos/')
+    seller_id=models.ForeignKey(Seller,on_delete=models.CASCADE, related_name='products')
+    category = models.CharField(max_length=100, blank=True, null=True)
